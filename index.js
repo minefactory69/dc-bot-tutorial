9d237da8949f5edeb23c0af45c6d1ed458c50e7e
minefactory69 <58185159+minefactory69@users.noreply.github.com>
2021-02-07 15:15:13 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const tokenfile = require("./tokenfile.json");
 const botconfig = require("./botconfig.json");
 const bot = new Discord.Client({disableEveryone: true});
 
+let botname = "Teszt bot"
+
 bot.on("ready", async() => {
     console.log(`${bot.user.username} elindult!`)
 
@@ -18,5 +20,51 @@ bot.on("ready", async() => {
         bot.user.setActivity(status, {type: "WATCHING"})
     }, 3000)
 })
+
+bot.on("message", async message => {
+    let MessageArray = message.content.split(" ");
+    let cmd = MessageArray[0];
+    let args = MessageArray.slice(1);
+    let prefix = botconfig.prefix;
+
+    if(cmd === `${prefix}hello`){
+        message.channel.send("Szia");
+    }
+
+
+    if(cmd === `${prefix}teszt`){
+        let TesztEmbed = new Discord.MessageEmbed()
+        .setColor("#98AA12")
+        .setAuthor(message.author.username)
+        .setTitle("Teszt Embed!")
+        .addField("Irodalom:", "Líra\n Epika\n dráma")
+        .setThumbnail(message.author.displayAvatarURL())
+        .setImage(message.guild.iconURL())
+        .setDescription(`\`${prefix}\``)
+        .setFooter(`${botname} | ${message.createdAt}`)
+
+        message.channel.send(TesztEmbed)
+    }
+
+    if(cmd === `${prefix}szöveg`){
+        let szöveg = args.join(" ");
+
+        if(szöveg) {
+            let dumaEmbed = new Discord.MessageEmbed()
+        .setColor("#98AA12")
+        .setAuthor(message.author.username)
+        .addField("Szöveg:", szöveg)
+        .setFooter(`${botname} | ${message.createdAt}`)
+    
+        message.channel.send(dumaEmbed)
+        } else {
+            message.reply("írj szöveget!")
+        }
+    }
+
+})
+
+
+
 
 bot.login(tokenfile.token);
